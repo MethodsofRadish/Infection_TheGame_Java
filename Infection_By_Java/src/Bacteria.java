32ae3bba036b62cd45693ffea0fc131a337de22b
Radish <wiensmarcus@gmail.com>
2020-09-16 19:55:21 -0500

    Add files via upload

---
+++ Infection_By_Java/src/Bacteria.java
@@ -0,0 +1,7 @@
+/**
+ * This class is used to create the game character's utilized by the player.
+ */
+public class Bacteria {
+    private double atp;
+
+}
---
+++ Infection_By_Java/src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+import package.Bacteria;
+
+public static void main(String[] args)
+        {
+            FINAL double STARTING_ATP = 50;
+
+            Bacteria player = new Bacteria(STARTING_ATP);
+        }
